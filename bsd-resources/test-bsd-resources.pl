d0c457b0575881456b680d559ae54914f4844c7d
Pedro Melo <melo@simplicidade.org>
2009-10-09 07:19:40 +0100

    Add metrics about CPU usage
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- bsd-resources/test-bsd-resources.pl
+++ bsd-resources/test-bsd-resources.pl
@@ -3,14 +3,34 @@
 use strict;
 use warnings;
 use BSD::Resource;
+use Time::HiRes qw( gettimeofday tv_interval );
+
+my $t0;
+my $r0;
 
 sub check_resources {
   my $desc  = shift;
   my @names = qw( utime stime maxrss ixrss idrss isrss minflt majflt
     nswap inblock oublock msgsnd msgrcv nsignals nvcsw nivcsw );
-  my $usage = getrusage();
 
-  print "Resources ($desc):";
+  my $usage = getrusage();
+  
+  my ($dt, $dr, $t1, $r1);
+  $t1 = [gettimeofday()];
+  $dt = tv_interval($t0, $t1) if defined $t0;
+  $t0 = $t1;
+  
+  $r1 = $usage->utime + $usage->stime;
+  $dr = $r1 - $r0 if defined $r0;
+  $r0 = $r1;
+
+  print "\nResources";
+  
+  printf(', real %0.4f', $dt) if defined $dt;
+  printf(', CPU %0.4f', $dr)  if defined $dr;
+  printf(', usage %0.4f%%', $dr/$dt*100) if defined $dr && defined $dt;
+  
+  print " ($desc):\n";
   for my $name (@names) {
     my $value = $usage->$name();
     print "  $name: $value\n" if $value;
