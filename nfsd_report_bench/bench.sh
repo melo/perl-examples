bc3bc29c868cebcf02fc64c476f512f168a8c6c1
Pedro Melo <melo@simplicidade.org>
2009-10-19 10:35:30 +0100

    Updated benchmark script
     * use the nfsd.gz file as input;
     * use our nfsd parsers;
     * include some timmings from previous runs.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- nfsd_report_bench/bench.sh
+++ nfsd_report_bench/bench.sh
@@ -1,18 +1,46 @@
 #!/bin/sh
 
 unset LANG
-FILE=xx.gz
+FILE=nfsd.gz
 
-echo gzcat pipe wc
-gzcat $FILE | time wc -lc
+echo my_stats
+gzcat $FILE | time ./my_stats_basic_optimized.pl
 
-echo gzcat pipe perl_wc
-gzcat $FILE | time ./wc_lines.pl
+echo system_perl my_stats
+gzcat $FILE | time /usr/bin/perl ./my_stats_basic_optimized.pl
 
-echo gzcat pipe system_perl perl_wc
-gzcat $FILE | time /usr/bin/perl ./wc_lines.pl
+echo base_line with system_perl
+gzcat $FILE | time /usr/bin/perl ./stats_basic_optimized.pl
 
 
+## source nfsd.gz
+
+## my_stats 5.10.1
+## lines 12236390 bytes 1073743047
+## Size mega 1024 reads 
+## Lines/sec 96588 MB/sec 8
+##       126.71 real       125.07 user         0.98 sys
+## 
+## my_stats system_perl 5.8.8
+## lines 12236390 bytes 1073743047
+## Size mega 1024 reads 
+## Lines/sec 95987 MB/sec 8
+##       127.49 real       125.42 user         0.97 sys
+## 
+## base_line
+
+
+## base_line with system_perl
+##
+## lines 12236390 bytes 1073743047
+## Size mega 1024 reads 
+## Lines/sec 67785 MB/sec 5948171
+##      180.54 real       177.24 user         1.33 sys
+
+
+
+##### Old line counters, for reference
+##
 ## Source yy.gz (ls -laR)
 
 ## wc
@@ -25,8 +53,3 @@ gzcat $FILE | time /usr/bin/perl ./wc_lines.pl
 ## system_perl perl_wc
 ## 194501376 lines 8757706752 bytes 95.65 real 74.69 user 4.81 sys
 ## 2033469 lines per second, 87MB/sec
-
-
-## Source xx.gz (data file)
-
-## 12341864 lines 1073742171 bytes 14.52 real 13.74 user 0.39 sys
